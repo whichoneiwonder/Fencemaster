f3c9a233646f105d8d5c7d4b18149ca5a9d1c989
whichoneiwonder <whichoneiwonder@github.com>
2025-03-29 17:14:27 +1100

    Apply spotify/google formatting

--- src/main/java/com/whichoneiwonder/aiproj/fencemaster/avnishj/Corner.java
+++ src/main/java/com/whichoneiwonder/aiproj/fencemaster/avnishj/Corner.java
@@ -1,71 +1,64 @@
 package com.whichoneiwonder.aiproj.fencemaster.avnishj;
-/** Class for the Corner Cell which is a sub-class
- * of Cell. Inherits all attributes and methods
- */
+
+/** Class for the Corner Cell which is a sub-class of Cell. Inherits all attributes and methods */
 public class Corner extends Cell {
-	
-	private int cornerNum;
 
-	/** Constructor which sets the location and state
-	 *  of the Corner cell based on the inputs
-	 *  
-	 *  @param x = Row Location
-	 * @param y = Col Location
-	 * @param state = String value of what state Cell is in
-	 * @param parentBoard TODO
-	 * @param edgeNum = edgeNumber of the board
-	 *  			
-	 */
-	public Corner(int x, int y, int state, GameBoard parentBoard, int cornerNumber){
-		super(x, y, state, parentBoard);
-		
-		/*As Cell is a corner piece, 3 of its edges will be
-		  null. We can set the respective links with the corresponding
-		  edge side of the cell to null by knowing which side of
-		  the board we are on, denoted by edgeNum */
-		cornerNum = cornerNumber;
-		setLink(cornerNum,null);
-		setLink((cornerNum+1)%6,null);
-		setLink((cornerNum+5)%6,null);
-	}
+  private int cornerNum;
 
-	/**
-	 * @return the type of Cell it is
-	 */
-	@Override
-	public String toString() {
-		return "Corner";
-	}
-	
-	
-	/**
-	 * @return the minimum steps to an edge
-	 */
-	@Override
-	public int getDistanceToEdge(int edgeNum){
-		int addition = 1;
-		if((edgeNum + 3)%6 == this.cornerNum || (edgeNum + 4)%6 == this.cornerNum){
-			addition = 0;
-		}
-		int returnValue=0; 
-		switch(edgeNum){
-			case 0:
-				returnValue = this.getRow();
-			case 1:
-				returnValue = parentBoard.getDimension() - 1 + this.getRow() - this.getCol();
-			case 2:
-				returnValue = (2*parentBoard.getDimension() - 2) - this.getCol();
-			case 3:
-				returnValue = (2*parentBoard.getDimension() - 2) - this.getRow();
-			case 4:
-				returnValue = (parentBoard.getDimension() - 1) + this.getCol() - this.getRow();
-			case 5:
-				returnValue = this.col;
-		}
-		return returnValue + addition;
-		
-	}
-	
-	
+  /**
+   * Constructor which sets the location and state of the Corner cell based on the inputs
+   *
+   * @param x = Row Location
+   * @param y = Col Location
+   * @param state = String value of what state Cell is in
+   * @param parentBoard TODO
+   * @param edgeNum = edgeNumber of the board
+   */
+  public Corner(int x, int y, int state, GameBoard parentBoard, int cornerNumber) {
+    super(x, y, state, parentBoard);
+
+    /*As Cell is a corner piece, 3 of its edges will be
+    null. We can set the respective links with the corresponding
+    edge side of the cell to null by knowing which side of
+    the board we are on, denoted by edgeNum */
+    cornerNum = cornerNumber;
+    setLink(cornerNum, null);
+    setLink((cornerNum + 1) % 6, null);
+    setLink((cornerNum + 5) % 6, null);
+  }
+
+  /**
+   * @return the type of Cell it is
+   */
+  @Override
+  public String toString() {
+    return "Corner";
+  }
+
+  /**
+   * @return the minimum steps to an edge
+   */
+  @Override
+  public int getDistanceToEdge(int edgeNum) {
+    int addition = 1;
+    if ((edgeNum + 3) % 6 == this.cornerNum || (edgeNum + 4) % 6 == this.cornerNum) {
+      addition = 0;
+    }
+    int returnValue = 0;
+    switch (edgeNum) {
+      case 0:
+        returnValue = this.getRow();
+      case 1:
+        returnValue = parentBoard.getDimension() - 1 + this.getRow() - this.getCol();
+      case 2:
+        returnValue = (2 * parentBoard.getDimension() - 2) - this.getCol();
+      case 3:
+        returnValue = (2 * parentBoard.getDimension() - 2) - this.getRow();
+      case 4:
+        returnValue = (parentBoard.getDimension() - 1) + this.getCol() - this.getRow();
+      case 5:
+        returnValue = this.col;
+    }
+    return returnValue + addition;
+  }
 }
-
--- src/main/java/com/whichoneiwonder/aiproj/fencemaster/avnishj/JamesAvnish.java
+++ src/main/java/com/whichoneiwonder/aiproj/fencemaster/avnishj/JamesAvnish.java
@@ -1,321 +1,279 @@
 package com.whichoneiwonder.aiproj.fencemaster.avnishj;
 
- 
-import java.util.Random;
-import java.io.PrintStream;
-
 import com.whichoneiwonder.aiproj.fencemaster.Move;
 import com.whichoneiwonder.aiproj.fencemaster.Piece;
 import com.whichoneiwonder.aiproj.fencemaster.Player;
+import java.io.PrintStream;
+import java.util.Random;
 
 public class JamesAvnish implements Player, Piece {
-	
-	private static final boolean DEBUG = false;
-	protected boolean opponentFirstMove = true;
-	protected GameBoard currentBoard;
-	protected int playerNum;
-	protected int enemyNum;
-	protected int turnNum;
-	protected final int MAXDEPTH = 5;
-	
-	
-	protected Random randNum;
-	
-	@Override
-public int getWinner() {
-		
-		//Win Checking Booleans
-		boolean blackWins = false;
-		boolean whiteWins = false;
-		boolean tripod = false;
-		boolean loop = false;
-		boolean blankSpotsOnBoard = currentBoard.checkForBlank();
-		
-		if (DEBUG)
-			currentBoard.printBoard();
-		
-		//Checks if Black has won by a tripod
-		if(currentBoard.checkTripod(BLACK)){
-			blackWins = true;
-			tripod = true;
-		}
-		
-		//Checks if White has won by a tripod
-		if(currentBoard.checkTripod(WHITE)){
-			whiteWins = true;
-			tripod = true;
-		}
-		
-		//Checks if Black has won by a Loop
-		if(currentBoard.checkLoop(BLACK)){
-			blackWins = true;
-			loop = true;
-		}
-		
-		//Checks if White has won by a loop
-		if(currentBoard.checkLoop(WHITE)){
-			whiteWins = true;
-			loop = true;
-		}
-		
-		printDebugWinnerStates(blackWins, whiteWins, tripod, loop, blankSpotsOnBoard);
-		
-		if(blackWins)
-			return BLACK;
-		
-		if(whiteWins)
-			return WHITE;
-		
-		if(blankSpotsOnBoard)
-			return INVALID;
-		
-		return EMPTY;
-	}
 
-	public void printDebugWinnerStates(boolean blackWins, boolean whiteWins, 
-			boolean tripod, boolean loop, boolean blankSpotsOnBoard){
-		
-		//Handles Printing given boolean values
-		
-		//If Both Players have Winning States
-		if (whiteWins && blackWins || !blankSpotsOnBoard){
-			System.out.println("Draw");
-			if (tripod && loop){
-				System.out.println("Both");
-			}
-			else{
-				if (tripod)
-					System.out.println("Tripod");
-				else if (!blankSpotsOnBoard){
-					System.out.println("Nil");
-				}else
-					System.out.println("Loop");
-				}
-			}
-				
-		//If White has won
-		else if (whiteWins){
-			System.out.println("White");
-			if (tripod && loop){
-				System.out.println("Both");
-			}
-			else{
-				if (tripod)
-					System.out.println("Tripod");
-				else
-					System.out.println("Loop");
-			}
-		}
-				
-		else if (blackWins){
-			System.out.println("Black");
-			if (tripod && loop){
-				System.out.println("Both");
-			}
-			else{
-				if (tripod)
-					System.out.println("Tripod");
-				else
-					System.out.println("Loop");
-				}
-		}
-		else{
-			System.out.println("None");
-			System.out.println("Nil");
-		}
-		
-		
-	}
+  private static final boolean DEBUG = false;
+  protected boolean opponentFirstMove = true;
+  protected GameBoard currentBoard;
+  protected int playerNum;
+  protected int enemyNum;
+  protected int turnNum;
+  protected final int MAXDEPTH = 5;
 
-	@Override
-	public int init(int n, int p) {
-		currentBoard = new GameBoard(n);
-		playerNum = p;
-		enemyNum = p%2+1;
-		turnNum = 1;
-		randNum = new Random();
-		
-		return 0;
-	}
+  protected Random randNum;
 
-	@Override
-	public Move makeMove() {
-		
-		Move nextMove = new Move();
-		turnNum++;
-		if(turnNum <3){
-			nextMove.IsSwap = false;
-			nextMove.P = playerNum;
-			if(currentBoard.getCellState(1, 1)== enemyNum){
-				nextMove.Col = 1;
-				nextMove.Row = currentBoard.getDimension()-1;
-				
-						
-			}else{
-				nextMove.Row=1;
-				nextMove.Col=1;
-			}
-			addMoveToBoard(nextMove);
-			return nextMove;
-		}
-		
-		nextMove = miniMax(MAXDEPTH);
-		
-		if(nextMove == null){
-			System.err.println("random move...");
-			nextMove = new Move();
-			nextMove.IsSwap = false;
-			nextMove.P = this.playerNum;
-			Cell target;
-			target = null;
-			target = currentBoard.getCellList(EMPTY).get(
-					randNum.nextInt(
-						currentBoard.getCellList(EMPTY).size()));
-			
-				
-			nextMove.Col = target.col;
-			nextMove.Row = target.row;
-			
-		}
-			
-		addMoveToBoard(nextMove);
-		System.out.flush();
-		System.err.println("Board Evaluation: " + 
-				currentBoard.eval(playerNum));
-		System.err.flush();
-		return nextMove;
-		
-		
-	}
+  @Override
+  public int getWinner() {
 
-	@Override
-	public int opponentMove(Move m) {
-		
-		Cell targetCell = currentBoard.getCell(m.Row, m.Col);
-		if(m.P == playerNum){
-			return INVALID;
-		}else if (m.P > BLACK || m.P < WHITE){
-			return INVALID;
-		}
-		if(targetCell == null){
-			return INVALID;
-		}
-		
-		 
-		if(m.IsSwap){
-			
-			if(!opponentFirstMove || m.P != BLACK){
-				return INVALID;
-			}
-			if(targetCell.getState() != WHITE){
-				return INVALID;
-			}
-			
-		}
-		
-		else if(targetCell.getState() != EMPTY){
-			return INVALID;
-		}
-		//set the target cell as placed by the opponent 
-		
-		turnNum++;
-		addMoveToBoard(m);
-		opponentFirstMove = false;
-		return 0;
-	}
+    // Win Checking Booleans
+    boolean blackWins = false;
+    boolean whiteWins = false;
+    boolean tripod = false;
+    boolean loop = false;
+    boolean blankSpotsOnBoard = currentBoard.checkForBlank();
 
-	@Override
-	public void printBoard(PrintStream output) {
-		currentBoard.printBoard(output);
+    if (DEBUG) currentBoard.printBoard();
 
-	}
-	public void addMoveToBoard(Move m){
-		
-		currentBoard.addMove(m);
-	}
+    // Checks if Black has won by a tripod
+    if (currentBoard.checkTripod(BLACK)) {
+      blackWins = true;
+      tripod = true;
+    }
 
-	public Move miniMax(int depth){
-		double maxVal = Double.MIN_VALUE;
-		double minVal;
-		Move m;
-		Move bestMove = null;
-		
-		for(Cell target : currentBoard.borderCellList.get(playerNum)){
-			m = new Move(playerNum , false, target.row, target.col);
-			//System.out.println(m);
-			minVal =  minMove(new GameBoard(currentBoard, m), depth -1 );
-			//System.out.println("MinVal: " + minVal);
-			if (maxVal <= minVal){
-				maxVal = minVal;
-				bestMove = m;
-			}
-			
-		}
-		return bestMove;
-	}
-	
-	protected double minMove (GameBoard parentBoard, int depth){
-		
-		if(parentBoard.checkTripod(playerNum)|| parentBoard.checkLoop(playerNum)){
-			return Double.MAX_VALUE;
-		}
-		if (depth <=0 ){
-			double eval = parentBoard.eval(playerNum);
-			//System.out.println("MinMove() Eval at depth " + depth + " eval: " + eval);
-			return eval - parentBoard.eval(enemyNum);
-		}
-		
-		
-		double minVal = Double.MAX_VALUE;
-		double maxVal;
-		Move m;
-		for(Cell target : parentBoard.borderCellList.get(enemyNum)){
-			m = new Move(enemyNum , false, target.row, target.col);
-			if (minVal > (maxVal =  maxMove(new GameBoard(parentBoard, m),depth-1))){
-				minVal = maxVal;
-			}
-			
-			
-		}
-		return minVal;
-	}
-		
-		protected double maxMove (GameBoard parentBoard, int depth){
-			if(parentBoard.checkTripod(enemyNum) || parentBoard.checkLoop(enemyNum)){
-				return Double.MIN_VALUE;
-			}
-			
-			
-			if (depth <=0 ){
-				double eval = parentBoard.eval(playerNum);
-				//System.out.println("MaxMove() Eval at depth " + depth + " eval: " + eval);
-				return eval - parentBoard.eval(enemyNum);
-			}
-			
-			
-			double maxVal = Double.MIN_VALUE;
-			double minVal;
-			Move m;
-			for(Cell target : parentBoard.borderCellList.get(playerNum)){
-				m = new Move(playerNum , false, target.row, target.col);
-				minVal =  minMove(new GameBoard(parentBoard, m), depth -1 );
-				//System.out.println("MaxMove() MinVal: " + minVal);
-				if (maxVal > minVal){
-					maxVal = minVal;
-				}
-				
-			}
-		
-		
-		
-		return maxVal;
-		
-	}
+    // Checks if White has won by a tripod
+    if (currentBoard.checkTripod(WHITE)) {
+      whiteWins = true;
+      tripod = true;
+    }
 
+    // Checks if Black has won by a Loop
+    if (currentBoard.checkLoop(BLACK)) {
+      blackWins = true;
+      loop = true;
+    }
 
+    // Checks if White has won by a loop
+    if (currentBoard.checkLoop(WHITE)) {
+      whiteWins = true;
+      loop = true;
+    }
 
+    printDebugWinnerStates(blackWins, whiteWins, tripod, loop, blankSpotsOnBoard);
 
-	
+    if (blackWins) return BLACK;
 
+    if (whiteWins) return WHITE;
 
+    if (blankSpotsOnBoard) return INVALID;
+
+    return EMPTY;
+  }
+
+  public void printDebugWinnerStates(
+      boolean blackWins,
+      boolean whiteWins,
+      boolean tripod,
+      boolean loop,
+      boolean blankSpotsOnBoard) {
+
+    // Handles Printing given boolean values
+
+    // If Both Players have Winning States
+    if (whiteWins && blackWins || !blankSpotsOnBoard) {
+      System.out.println("Draw");
+      if (tripod && loop) {
+        System.out.println("Both");
+      } else {
+        if (tripod) System.out.println("Tripod");
+        else if (!blankSpotsOnBoard) {
+          System.out.println("Nil");
+        } else System.out.println("Loop");
+      }
+    }
+
+    // If White has won
+    else if (whiteWins) {
+      System.out.println("White");
+      if (tripod && loop) {
+        System.out.println("Both");
+      } else {
+        if (tripod) System.out.println("Tripod");
+        else System.out.println("Loop");
+      }
+    } else if (blackWins) {
+      System.out.println("Black");
+      if (tripod && loop) {
+        System.out.println("Both");
+      } else {
+        if (tripod) System.out.println("Tripod");
+        else System.out.println("Loop");
+      }
+    } else {
+      System.out.println("None");
+      System.out.println("Nil");
+    }
+  }
+
+  @Override
+  public int init(int n, int p) {
+    currentBoard = new GameBoard(n);
+    playerNum = p;
+    enemyNum = p % 2 + 1;
+    turnNum = 1;
+    randNum = new Random();
+
+    return 0;
+  }
+
+  @Override
+  public Move makeMove() {
+
+    Move nextMove = new Move();
+    turnNum++;
+    if (turnNum < 3) {
+      nextMove.IsSwap = false;
+      nextMove.P = playerNum;
+      if (currentBoard.getCellState(1, 1) == enemyNum) {
+        nextMove.Col = 1;
+        nextMove.Row = currentBoard.getDimension() - 1;
+
+      } else {
+        nextMove.Row = 1;
+        nextMove.Col = 1;
+      }
+      addMoveToBoard(nextMove);
+      return nextMove;
+    }
+
+    nextMove = miniMax(MAXDEPTH);
+
+    if (nextMove == null) {
+      System.err.println("random move...");
+      nextMove = new Move();
+      nextMove.IsSwap = false;
+      nextMove.P = this.playerNum;
+      Cell target;
+      target = null;
+      target =
+          currentBoard
+              .getCellList(EMPTY)
+              .get(randNum.nextInt(currentBoard.getCellList(EMPTY).size()));
+
+      nextMove.Col = target.col;
+      nextMove.Row = target.row;
+    }
+
+    addMoveToBoard(nextMove);
+    System.out.flush();
+    System.err.println("Board Evaluation: " + currentBoard.eval(playerNum));
+    System.err.flush();
+    return nextMove;
+  }
+
+  @Override
+  public int opponentMove(Move m) {
+
+    Cell targetCell = currentBoard.getCell(m.Row, m.Col);
+    if (m.P == playerNum) {
+      return INVALID;
+    } else if (m.P > BLACK || m.P < WHITE) {
+      return INVALID;
+    }
+    if (targetCell == null) {
+      return INVALID;
+    }
+
+    if (m.IsSwap) {
+
+      if (!opponentFirstMove || m.P != BLACK) {
+        return INVALID;
+      }
+      if (targetCell.getState() != WHITE) {
+        return INVALID;
+      }
+
+    } else if (targetCell.getState() != EMPTY) {
+      return INVALID;
+    }
+    // set the target cell as placed by the opponent
+
+    turnNum++;
+    addMoveToBoard(m);
+    opponentFirstMove = false;
+    return 0;
+  }
+
+  @Override
+  public void printBoard(PrintStream output) {
+    currentBoard.printBoard(output);
+  }
+
+  public void addMoveToBoard(Move m) {
+
+    currentBoard.addMove(m);
+  }
+
+  public Move miniMax(int depth) {
+    double maxVal = Double.MIN_VALUE;
+    double minVal;
+    Move m;
+    Move bestMove = null;
+
+    for (Cell target : currentBoard.borderCellList.get(playerNum)) {
+      m = new Move(playerNum, false, target.row, target.col);
+      // System.out.println(m);
+      minVal = minMove(new GameBoard(currentBoard, m), depth - 1);
+      // System.out.println("MinVal: " + minVal);
+      if (maxVal <= minVal) {
+        maxVal = minVal;
+        bestMove = m;
+      }
+    }
+    return bestMove;
+  }
+
+  protected double minMove(GameBoard parentBoard, int depth) {
+
+    if (parentBoard.checkTripod(playerNum) || parentBoard.checkLoop(playerNum)) {
+      return Double.MAX_VALUE;
+    }
+    if (depth <= 0) {
+      double eval = parentBoard.eval(playerNum);
+      // System.out.println("MinMove() Eval at depth " + depth + " eval: " + eval);
+      return eval - parentBoard.eval(enemyNum);
+    }
+
+    double minVal = Double.MAX_VALUE;
+    double maxVal;
+    Move m;
+    for (Cell target : parentBoard.borderCellList.get(enemyNum)) {
+      m = new Move(enemyNum, false, target.row, target.col);
+      if (minVal > (maxVal = maxMove(new GameBoard(parentBoard, m), depth - 1))) {
+        minVal = maxVal;
+      }
+    }
+    return minVal;
+  }
+
+  protected double maxMove(GameBoard parentBoard, int depth) {
+    if (parentBoard.checkTripod(enemyNum) || parentBoard.checkLoop(enemyNum)) {
+      return Double.MIN_VALUE;
+    }
+
+    if (depth <= 0) {
+      double eval = parentBoard.eval(playerNum);
+      // System.out.println("MaxMove() Eval at depth " + depth + " eval: " + eval);
+      return eval - parentBoard.eval(enemyNum);
+    }
+
+    double maxVal = Double.MIN_VALUE;
+    double minVal;
+    Move m;
+    for (Cell target : parentBoard.borderCellList.get(playerNum)) {
+      m = new Move(playerNum, false, target.row, target.col);
+      minVal = minMove(new GameBoard(parentBoard, m), depth - 1);
+      // System.out.println("MaxMove() MinVal: " + minVal);
+      if (maxVal > minVal) {
+        maxVal = minVal;
+      }
+    }
+
+    return maxVal;
+  }
 }
